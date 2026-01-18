e1c12ed141cffa8e03ee9317776c2539dcf27bfc
Mohammed Rayan A <111146442+themohammedrayan@users.noreply.github.com>
2026-01-18 23:53:35 +0530

    new

--- backend/app.py
+++ backend/app.py
@@ -180,7 +180,12 @@ def exam_result_search():
     return jsonify(r.json().get("data"))
 
 
-
 if __name__ == "__main__":
-    app.run(port=5000, debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
+
+
+#if __name__ == "__main__":
+ #   app.run(port=5000, debug=True)
 
